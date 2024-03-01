9c39ce95a56cf11acc59e81251448044f1b8b9dd
ritesh.rathore <ritesh.rathore>
2024-03-01 12:54:14 +0530

    Jai Hanuman

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter The Complete Guide',
+      title: 'Flutter The Complete Guide || Jai Hanuman ||',
       theme: ThemeData(
         // This is the theme of your application.
         //
